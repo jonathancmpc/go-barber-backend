0868bf8885695435eefbeaa4d107a4381577fd55
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-06-29 19:48:59 -0300

    M-U Revert "Configurando banco de dados"
    This reverts commit 2da6204b19f66b75e1e262d70d92a38f379095fc.

--- src/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import appointmentsRouter from './appointments.routes';
-
-const routes = Router();
-
-/* Toda rota que utilizar /appointments passará para o controlador de rotas que está em ./appointments.routes, independente se for post, get... */
-routes.use('/appointments', appointmentsRouter);
-
-export default routes;
