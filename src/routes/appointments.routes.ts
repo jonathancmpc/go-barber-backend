62ea2bf5682e125de90e2aa028f0ee4ad5d813ab
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-06-30 09:26:21 -0300

    Migration,Model,Decorators configurados Appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,36 +1,31 @@
 import { Router } from 'express';
+import { getCustomRepository } from 'typeorm';
 import { parseISO } from 'date-fns';
 
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
-const createAppointmentService = new CreateAppointmentService(
-  appointmentsRepository,
-);
 
 /* A Rota deve estar preocupada apenas com: receber a requisição, chamar outro arquivo para tratar essa requisição e devolver a resposta para o cliente. */
 
 /* Listando os agendamentos */
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return response.json(appointments);
 });
 
-/* Como temos o método use dentro de index.ts que já nos dá a rota com /appointments, não precisamos utiliza-la aqui nessas rotas, por já cai vir automáticamente, bastando passar o próximo item depois da barra */
-appointmentsRouter.post('/', (request, response) => {
-  /* Retornando o erro caso já tenha agendamento, o erro foi codificado dentro do service com o throw */
+appointmentsRouter.post('/', async (request, response) => {
   try {
-    /* Pega o prestador de serviço(barbeiro) e a data hora do agendamento */
     const { provider, date } = request.body;
 
-    // A transformação dos dados não é regra de negócio(parse), porém a manipulação da data sim.
     const parsedDate = parseISO(date);
 
-    // Chamando o Service com a regra de negócio e passando o repositório como parãmetro
-    const appointment = createAppointmentService.execute({
+    const createAppointmentService = new CreateAppointmentService();
+
+    const appointment = await createAppointmentService.execute({
       provider,
       date: parsedDate,
     });
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,14 +1,8 @@
 import { startOfHour } from 'date-fns';
 
+import { getCustomRepository } from 'typeorm';
 import Appointment from '../models/Appointment';
-import appointmentsRepository from '../repositories/AppointmentsRepository';
-import AppointmentRepository from '../repositories/AppointmentsRepository';
-
-/* Problemas para resolver no código:
-[ X ] Recebimento das informações
-[ X ] Tratativas de erros e exceções (Como não temos acesso ao request e response, a única coisa que podemos fazer é implementar o thow. E migramos a resposta ao cliente com o status para a rota)
-[ X ] Acesso ao repositório
-*/
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
 interface RequestDTO {
   provider: string;
@@ -16,18 +10,13 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-  /* Para utilizarmos a mesma instância de repositórios sempre, e não ter erro de ficar criando em cada service um novo repositório(fazendo com que tenhamos repositórios diferentes), instânciamos então através do constructor, passando o repositório como parâmetro. Então importamos o repositório para falar que o tipo do nosso parâmetro é appointmentRepository. ATENÇÃO: quando o Service for chamado na rota, deve-se passar esse parâmetro(repositório) */
-  private appointmentsRepository: AppointmentRepository;
+  public async execute({ provider, date }: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: appointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository; /* esse é o que estamos recebendo no constructor como parâmentro, que é o real appointmentsrepository */
-  }
-
-  public execute({ provider, date }: RequestDTO): Appointment {
     const appointmentDate = startOfHour(date);
 
     /* Verifica se existe alguma data igual dentro do nosso banco de dados ficticio, se tiver, ele retorna essa data dentro da variável findAppointmentInSameDate, e se não encontrar nenhuma, retorna false */
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -36,11 +25,13 @@ class CreateAppointmentService {
       // O código de retorno de status foi migrado para rota.
     }
 
-    /* Enviando os parâmetros NOMEADOS como objetos obedecendo o conceito de DTO */
-    const appointment = this.appointmentsRepository.create({
+    /* Não é necessário ser assíncrono pq ele só cria, quem salva no banco é o save */
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
