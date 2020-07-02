3175247c40a393c66b602e7abbce03235702c83e
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-07-02 09:46:05 -0300

    Regra de Negocio de Upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,17 @@ import path from 'path';
 import multer from 'multer';
 import crypto from 'crypto';
 
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
+
 export default {
+  // Essa variável foi passada apenas para recuperarmos o caminho do diretório quando chamarmos a função
+  directory: tmpFolder,
   // Por enquanto vamos salvar os uploads em nossa aplicação, dentro da pasta tmp na raiz do projeto
   storage: multer.diskStorage({
-    destination: path.resolve(__dirname, '..', '..', 'tmp'),
+    destination: tmpFolder,
     filename(request, file, callback) {
       // Criando um hash para o nome do arquivo, para assegurar que não vai haver arquivos com nomes repetidos. Então utilizamos o crypto passando 10bytes de texto aleatório e transformamos em formato hexadecimal.
-      const fileHash = crypto.randomBytes(10).toString('HEX');
+      const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
       // Seocorrer algum erro no salvamento do arquivo, ele dará o callback
       return callback(null, fileName);
---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,46 @@
+import { getRepository } from 'typeorm';
+import path from 'path';
+import fs from 'fs'; // fileSystem do Node
+
+import User from '../models/User';
+import uploadConfig from '../config/upload';
+
+interface Request {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<User> {
+    const usersRepository = getRepository(User);
+
+    // verificando se o id de usuário é válido
+    const user = await usersRepository.findOne(user_id);
+
+    if (!user) {
+      throw new Error('Only authenticated users can change avatar.');
+    }
+
+    // verifica se o usuário já tem um avatar para deletar o arquivo antigo
+    if (user.avatar) {
+      // fazendo o join entre dois caminhos
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      console.log(userAvatarFilePath);
+      // Verificando se existe o avatar, o stat do fileSystem retorna o status do path somente se ele existir, então com isso podemos verificar se ele existe ou não.
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    // Atualizando o usuário
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user); // o método save -> se já existir o usuário ele atualiza, e se não existir ele cria.
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
