eaf82525f802eab73e682b36b931c57d61d24ce7
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-08-24 19:44:07 -0300

    Primeiro teste com repositorio fake

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,4 +1,25 @@
-test('Soma de dois números', () => {
-  /* Espera-se que a soma de 1 + 2 retorne 3 */
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointments', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      date: new Date(),
+      provider_id: '123123131',
+    });
+
+    /* Espera que o appointment contenha um id */
+    expect(appointment).toHaveProperty('id');
+    /* Espera que o appointment contenha um provider_id */
+    expect(appointment.provider_id).toBe('123123131');
+  });
+
+  /* it('should not be able to create two appointments on the same time', () => {
+
+  }); */
 });
