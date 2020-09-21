588c408b1eae41d70324391dbb6b743781c49c3d
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-09-21 12:03:51 -0300

    Listagem de prestadores

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appointmentsRouter from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRouter from '@modules/appointments/infra/http/routes/providers.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
@@ -10,6 +11,7 @@ const routes = Router();
 
 /* Toda rota que utilizar /appointments passará para o controlador de rotas que está em ./appointments.routes, independente se for post, get... */
 routes.use('/appointments', appointmentsRouter);
+routes.use('/providers', providersRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
